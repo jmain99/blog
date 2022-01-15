29b0f296c19391bb2a224fba8e0e6bed5791d593
Jmain <jailaomain@gmail.com>
2022-01-15 20:20:36 +0630

    Heroku config files and updates

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,10 @@ from functools import cmp_to_key
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic import CreateView
 # Create your views here.
 
-class SignupView(generic.CreateView):
+class SignupView(CreateView):
     form_class=UserCreationForm
     success_url=reverse_lazy('login')
     template_name='signup.html'
